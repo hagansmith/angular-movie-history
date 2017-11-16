f15a4487203c16ef518df516213bac8bc9cb7650
Adam Smith <hagansmith@gmail.com>
2017-11-15 20:56:32 -0600

    save rated movies to firebase

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,7 +1,18 @@
 "use strict";
 
-app.controller("SearchCtrl", function($scope, tmdbService){
+app.controller("SearchCtrl", function($location, $rootScope, $scope, MovieService, tmdbService){
   $scope.movies = [];
+
+const createMovie = (movie) => {
+  return {
+    "title": movie.title,
+    "overview": movie.overview,
+    "poster_path": movie.poster_path,
+    "rating": 0,
+    "isWatched": true,
+    "uid": $rootScope.uid
+  };
+};
 
 $scope.enterPush = (event) => {
   if (event.keyCode === 13){
@@ -12,4 +23,14 @@ $scope.enterPush = (event) => {
       });
     }
   };
+
+$scope.saveRated = (tmdbMovie) => {
+  let newMovie = createMovie(tmdbMovie);
+  MovieService.postNewMovie(newMovie).then(()=>{
+    $location.path('/rated');
+  }).catch((err)=>{
+    console.log("error in postNewMovie", err);
+  });
+};
+
 });
